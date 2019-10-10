fc2208323c07dffe88aa0fc279aaf7a34b222c3b
unknown <j.currier224@gmail.com>
2019-10-10 15:26:25 -0700

    daily exercise

--- src/JavaScript/Daily/daily.test.js
+++ src/JavaScript/Daily/daily.test.js
@@ -1,17 +1,29 @@
-import assertEquals from './daily.js'
+import functions from './daily.js'
 
-test('check parameters', () => {
-    expect(assertEquals("a","b")).toBe(false); // Consider the edge cases
-    expect(assertEquals("a","a")).toBe(true);
-    expect(assertEquals(1,2)).toBe(false);
-    expect(assertEquals(2,2)).toBe(true); 
-    expect(assertEquals("2",2)).toBe(false);
-    expect(assertEquals("This value","This value")).toBe(true);
 
+// //2019-10-6 Daily Exercise
+// test('check parameters', () => {
+//     expect(funtions.assertEquals("a","b")).toBe(false); // Consider the edge cases
+//     expect(functions.assertEquals("a","a")).toBe(true);
+//     expect(functions.assertEquals(1,2)).toBe(false);
+//     expect(functions.assertEquals(2,2)).toBe(true); 
+//     expect(functions.assertEquals("2",2)).toBe(false);
+//     expect(functions.assertEquals("This value","This value")).toBe(true);
+
+// });
+
+// test('practice test', () => {
+//     console.log("hello world");
+
+// });
+
+//2019-10-9 Daily Exercise
+test('email builder from an array', () => {
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
 });
-
-test('practice test', () => {
-    console.log("hello world");
-
-});
-
