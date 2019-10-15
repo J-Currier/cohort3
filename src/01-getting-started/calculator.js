1223fcee679c4a0c4b3b343b28a28d97bd2e636c
unknown <j.currier224@gmail.com>
2019-10-15 12:15:04 -0700

    JS rename

--- src/01-getting-started/calculator.js
+++ src/01-getting-started/calculator.js
@@ -5,6 +5,27 @@ const calculator = {
     operator: null,
 };
 
+function inputDigit(digit) {
+    const { displayValue } = calculator;
+    calculator.displayValue = displayValue === "0" ? digit: displayValue + digit;
+};
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    };
+};
+
+function handleOperator(nextOperator) {
+    const {firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+    if (firstOperand === null) {
+        calculator.firstOperand = inputValue;
+    }
+    calculator.waitingForSecondOperand =true;
+    calculator.operator =nextOPerator;
+}
+
 function updateDisplay() {
     const display = document.querySelector('.result');
     display.value = calculator.displayValue;
@@ -13,3 +34,29 @@ function updateDisplay() {
 
 updateDisplay();
     
+const keys = document.querySelector('.buttons');
+
+keys.addEventListener('click', (event) => {
+    const { target} = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operation')) {
+        console.log('operator', target.value);
+        return;
+    }
+    if (target.classList.contains('clear')) {
+        console.log('clear', target.values);
+        return;
+    }
+    if (target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+// const { target } = event;
+// const target = event.target;
