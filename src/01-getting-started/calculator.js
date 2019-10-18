42a9fe02274ef883304a10f9f0c64f6049e74f3b
unknown <j.currier224@gmail.com>
2019-10-18 14:54:44 -0700

    Fixed decimal bug on tax calc

--- src/01-getting-started/calculator.js
+++ src/01-getting-started/calculator.js
@@ -1,123 +1,125 @@
-// const calculator = {
-//     displayValue: '0',
-//     firstOperand: null,
-//     waitingForSecondOperand: false,
-//     operator: null,
-// };
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
 
-// function inputDigit(digit) {
-//     const { displayValue, waitingForSecondOperand } = calculator;
-//     if (waitingForSecondOperand === true) {
-//         calculator.displayValue = digit;
-//         calculator.waitingForSecondOperand = false;
-//     } else { 
-//     calculator.displayValue = displayValue === "0" ? digit: displayValue + digit;
-//     };
-//     console.log(calculator);
-// };
+function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else { 
+    calculator.displayValue = displayValue === "0" ? digit: displayValue + digit;
+    };
+    console.log(calculator);
+};
 
-// function inputDecimal(dot) {
-//     if (calculator.waitingForSecondOperand === true) return;
-//     if (!calculator.displayValue.includes(dot)) {
-//         calculator.displayValue += dot;
-//     };
-// };
+function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) return;
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    };
+};
 
-// function handleOperator(nextOperator) {
-//     const {firstOperand, displayValue, operator } = calculator;
-//     const inputValue = parseFloat(displayValue);
-//     if (operator && calculator.waitingForSecondOperand) {
-//         calculator.operator = nextOperator;
-//         console.log(calculator);
-//         return;
-//     }
-//     if (firstOperand === null) {
-//         calculator.firstOperand = inputValue;
-//     } else if (operator) {
-//         const result = performCalculation[operator](firstOperand, inputValue);
-//     calculator.displayValue = String(result);
-//     calculator.firstOperand = result;
-//     }
-//     calculator.waitingForSecondOperand =true;
-//     calculator.operator = nextOperator;
-//     console.log(calculator);
-// }
+function handleOperator(nextOperator) {
+    const {firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        console.log(calculator);
+        return;
+    }
+    if (firstOperand === null) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = performCalculation[operator](firstOperand, inputValue);
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+    }
+    calculator.waitingForSecondOperand =true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+}
 
-// const performCalculation = {
-//     "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
-//     "x": (firstOperand, secondOperand) => firstOperand * secondOperand,
-//     "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
-//     "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
-//     "=": (firstOperand, secondOperand) => secondOperand,
-// };
+const performCalculation = {
+    "/": (firstOperand, secondOperand) => firstOperand / secondOperand,
+    "x": (firstOperand, secondOperand) => firstOperand * secondOperand,
+    "+": (firstOperand, secondOperand) => firstOperand + secondOperand,
+    "-": (firstOperand, secondOperand) => firstOperand - secondOperand,
+    "=": (firstOperand, secondOperand) => secondOperand,
+};
 
-// function percentageFunction() {
-//     calculator.displayValue = parseFloat(calculator.displayValue) / 100;
+function percentageFunction() {
+    calculator.displayValue = parseFloat(calculator.displayValue) / 100;
     
-// };
+};
 
-// function resetCalculator() {
-//     calculator.displayValue = "0";
-//     calculator.firstOperand = null;
-//     calculator.waitingForSecondOperand = false;
-//     calculator.operator = null;
-//     console.log(calculator);
-// }
+function resetCalculator() {
+    calculator.displayValue = "0";
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+    console.log(calculator);
+}
 
-// function updateDisplay() {
-//     const display = document.querySelector('.result');
-//     display.value = calculator.displayValue;
+function updateDisplay() {
+    const display = document.querySelector('.result');
+    display.value = calculator.displayValue;
 
-// }
+}
 
-// updateDisplay();
+updateDisplay();
     
-// const keys = document.querySelector('.buttons');
+const keys = document.querySelector('.buttons');
 
-// keys.addEventListener('click', (event) => {
-//     const { target} = event;
-//     if (!target.matches('button')) {
-//         return;
-//     }
-//     if (target.classList.contains('operation')) {
-//         handleOperator(target.value);
-//         updateDisplay();
-//         return;
-//     }
-//     if (target.classList.contains('clear')) {
-//         resetCalculator();
-//         updateDisplay();
-//         return;
-//     }
-//     if (target.classList.contains("decimal")) {
-//         inputDecimal(target.value);
-//         updateDisplay();
-//         return;
-//     }
-//     if (target.classList.contains("percentage")){
-//         percentageFunction();
-//         updateDisplay();
-//         return;
-//     }
-//     inputDigit(target.value);
-//     updateDisplay();
-// });
+keys.addEventListener('click', (event) => {
+    const { target} = event;
+    if (!target.matches('button')) {
+        return;
+    }
+    if (target.classList.contains('operation')) {
+        handleOperator(target.value);
+        updateDisplay();
+        return;
+    }
+    if (target.classList.contains('clear')) {
+        resetCalculator();
+        updateDisplay();
+        return;
+    }
+    if (target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+    if (target.classList.contains("percentage")){
+        percentageFunction();
+        updateDisplay();
+        return;
+    }
+    inputDigit(target.value);
+    updateDisplay();
+});
 
-// // VVVVVVVV Canadian Tax Calculator Code VVVVVVVV
+// VVVVVVVV Canadian Tax Calculator Code VVVVVVVV
 
 
 
-// myButton.addEventListener('click', function() {
-//     console.log("button was pressed");
-//     let textInput = document.getElementById("incomeinput").value;
-//     let totTaxPaid = testFunctions.taxCalcTaxPayment(textInput);
-//     document.getElementById("outputtaxesans").innerHTML = "$ " + totTaxPaid;
-//     let totIncome = testFunctions.netIncome(textInput);
-//     document.getElementById("outputincomeans").innerHTML = "$ " + totIncome;
-//     let totrate = testFunctions.effectiveTaxRate(textInput);
-//     let totalrate = Number.parseFloat(totrate).toFixed(2);
-//     document.getElementById("outputtaxrateans").innerHTML = totalrate + "%";
-// });
+myButton.addEventListener('click', function() {
+    console.log("button was pressed");
+    let textInput = document.getElementById("incomeinput").value;
+    let totTaxPaid = testFunctions.taxCalcTaxPayment(textInput);
+    let totalTaxPaid = Number.parseFloat(totTaxPaid).toFixed(2);
+    document.getElementById("outputtaxesans").innerHTML = "$ " + totalTaxPaid;
+    let totIncome = testFunctions.netIncome(textInput);
+    let totalIncome = Number.parseFloat(totIncome).toFixed(2);
+    document.getElementById("outputincomeans").innerHTML = "$ " + totalIncome;
+    let totrate = testFunctions.effectiveTaxRate(textInput);
+    let totalrate = Number.parseFloat(totrate).toFixed(2);
+    document.getElementById("outputtaxrateans").innerHTML = totalrate + "%";
+});
 
 
 // vvvv Array Code vvvv
