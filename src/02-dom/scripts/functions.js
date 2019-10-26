cabfd290d97a00c358bee825f05eb6758b32e726
unknown <j.currier224@gmail.com>
2019-10-26 12:34:05 -0700

    card buttons and add card

--- src/02-dom/scripts/functions.js
+++ src/02-dom/scripts/functions.js
@@ -1,3 +1,5 @@
+// import { arrowFunctionExpression } from "@babel/types";
+
 const functions = {
     divClickFunction: () => {
         let divPosition = event.target;
@@ -27,13 +29,50 @@ const functions = {
         let cardNumberArray = functions.cardIdArrayFunction();
         console.log(cardNumberArray);
         let highestCardNumber = Math.max(...cardNumberArray);
-        console.log(highestCardNumber);
+        console.log("highest cars is", highestCardNumber);
         let newDiv = functions.createNewCardFunction(highestCardNumber);
         console.log(newDiv);
     },
 
     createNewCardFunction: (highestCardNumber) => {
-        return "hi";
+        let newCardNumber = highestCardNumber + 1;
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'addedCards');
+        newDiv.setAttribute('id', newCardNumber)
+        newDiv.innerHTML = "Card " + newCardNumber + "<br />";
+        leftPanelCardsID.insertBefore(newDiv, leftPanelCardsID.childNodes[0]);
+        functions.createAddCardBeforeButton(newCardNumber);
+        functions.createAddCardAfterButton(newCardNumber); 
+        functions.createDeleteButton(newCardNumber); 
+        console.log(newDiv);
+        // leftPanelCardsID.insertBefore(newDiv, leftPanelCardsID.childNodes[0]);
+    },
+
+    createDeleteButton: (newCardNumber) => {
+        let button = document.createElement('button');
+        button.setAttribute('class', 'domButtons delete');
+        button.innerHTML = 'Delete Card';
+        button.id = "Delete";
+        let newCardID = document.getElementById(newCardNumber)
+        newCardID.appendChild(button);
+    },
+
+    createAddCardBeforeButton: (newCardNumber) => {
+        let button = document.createElement('button');
+        button.setAttribute('class', 'domButtons addCard');
+        button.innerHTML = 'Add Before';
+        button.id = "addBefore";
+        let newCardID = document.getElementById(newCardNumber)
+        newCardID.appendChild(button);
+    },
+
+    createAddCardAfterButton: (newCardNumber) => {
+        let button = document.createElement('button');
+        button.setAttribute('class', 'domButtons addCard');
+        button.innerHTML = 'Add After';
+        button.id = "addAfter";
+        let newCardID = document.getElementById(newCardNumber)
+        newCardID.appendChild(button);
     },
 
     cardIdArrayFunction: () => {
@@ -44,17 +83,6 @@ const functions = {
             cardNumberArray.push(cardChildren[i].id);
         };
         return cardNumberArray;
-
-
-    // const newDiv = document.createElement('div');
-    // newDiv.setAttribute('class', 'insideBoxes');
-    // newDiv.setAttribute('counter', counterNum)
-    // newDiv.textContent = "counter is " + counterNum;
-
-    // input.appendChild(newDiv);
-    // console.log(newDiv);
-    // return counterNum;
-
     },
 }
 
