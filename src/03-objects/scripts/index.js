1ac12f5edfc12ed159df5807397edc77d10e48fb
unknown <j.currier224@gmail.com>
2019-11-04 12:58:12 -0700

    comp130B balance function

--- src/03-objects/scripts/index.js
+++ src/03-objects/scripts/index.js
@@ -1,9 +1,15 @@
 import { Account } from "./account.js";
+// import { functionExpression } from "@babel/types";
 
 let savingsAccount = new Account('Savings Account', 0);
 // console.log(savingsAccount);
 
+
+
+
 depositButton.addEventListener('click', function() {
+    savingsAccount.deposit(document.getElementById('depositAmount').value)
+    document.getElementById('currentBalance').innerHTML = `Current Balance: $ ${parseInt(savingsAccount.balance).toFixed(2)}`
     console.log('deposit button clicked')
     console.log(savingsAccount);
 
