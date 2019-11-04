bda3badb1d45c64566d76d88fcb64c8a1da97859
unknown <j.currier224@gmail.com>
2019-11-04 13:20:24 -0700

    comp130b withdraw

--- src/03-objects/scripts/index.js
+++ src/03-objects/scripts/index.js
@@ -10,11 +10,13 @@ let savingsAccount = new Account('Savings Account', 0);
 depositButton.addEventListener('click', function() {
     savingsAccount.deposit(document.getElementById('depositAmount').value)
     document.getElementById('currentBalance').innerHTML = `Current Balance: $ ${parseInt(savingsAccount.balance).toFixed(2)}`
-    console.log('deposit button clicked')
-    console.log(savingsAccount);
+    document.getElementById('depositAmount').value = "";
 
 });
 
 withdrawlButton.addEventListener('click', function() {
-    console.log('withdrawl button clicked')
+    savingsAccount.withdraw(document.getElementById('withdrawAmount').value)
+    document.getElementById('currentBalance').innerHTML = `Current Balance: $ ${parseInt(savingsAccount.balance).toFixed(2)}`
+    document.getElementById("withdrawAmount").value = '';
+
 });
