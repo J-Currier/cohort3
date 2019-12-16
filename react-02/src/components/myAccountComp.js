3c925d14be98a964ddae7f8985c00012f39d6a54
unknown <j.currier224@gmail.com>
2019-12-16 15:29:22 -0700

    basic cards adding to page

--- react-02/src/components/myAccountComp.js
+++ react-02/src/components/myAccountComp.js
@@ -34,24 +34,19 @@ class MyAccountComp extends React.Component {
            accountList: this.state.accountList.concat(myNewAccount),
            counter: this.state.counter +1,
         });
-        console.log(myNewAccount)
-        console.log (this.state);
         this.setState({
             accountName: '',
             accountBalance: '',
         });
 
-        // DisplayAccounts(this.state.accountList);
     }
 
     handleNameChange(event) {
         this.setState({accountName: event.target.value});
-        console.log(this.state.accountName)
     }
 
     handleBalanceChange(event) {
         this.setState({accountBalance: event.target.value});
-        console.log(this.state.accountBalance)
     }
 
 
@@ -74,7 +69,6 @@ class MyAccountComp extends React.Component {
                             <div className='buttonDiv'>
                             
                             <NewAccountButton onClick={() => this.createNewAccount()} />    
-                            {/* button className='buttons' id='addAccountButton'>Make a New Account!</button> */}
                             </div>
                             <div id='displayArea'>
                             <div id='textdisplay'>
@@ -87,7 +81,6 @@ class MyAccountComp extends React.Component {
                     </div>
                     <div className='myAccountCards' id='myAccountCardsId'>
                         <DisplayAccounts myAccounts={this.state.accountList}/>
-                        {/* creat component that maps account list and renders */}
                     </div>
                 </div>
             </div>
@@ -104,14 +97,78 @@ function NewAccountButton(props) {
 
 
 function DisplayAccounts(props) {
-    console.log('display accounts');
-    // const myAccounts = this.state.accountList
-    const cardList = props.myAccounts.map((account) => <li key={account.uniqueID.toString()}> {account.name}</li>);
+const cardList = props.myAccounts.map((account) => <li key={account.uniqueID.toString()}> {CreateNewCardFunction(account)}</li>);
     return <ul>{cardList}</ul>;
 }
 
-// put creat card function here
+function CreateNewCardFunction(account) {
 
+    let currencyBalance = parseFloat(account.balance);
+    
+    return (
+
+        <div className="cards" id={account.name} key={account.uniqueID} >
+            <div className='accountCardHeader' >{account.name}</div>
+        </div>
+    )
+
+    
+
+// //     const newAccountCard = document.createElement('div');
+// //     newAccountCard.setAttribute('class', 'cards');
+// //     newAccountCard.setAttribute('id', newAccountName);
+// //     newAccountCard.setAttribute('key', counter);
+// //     console.log(newAccountCard);
+
+//     let titleDiv = document.createElement('div');
+//     titleDiv.setAttribute('class', 'accountCardHeader')
+//     let newId = newAccountName + 'Title';
+//     titleDiv.setAttribute('id', newId);
+//     titleDiv.innerHTML = newAccountName;
+//     newAccountCard.appendChild(titleDiv);
+
+//     let balanceDiv = document.createElement('div');
+//     balanceDiv.setAttribute('class', 'accountCardBalance')
+//     let newBalId = newAccountName + 'Balance';
+//     balanceDiv.setAttribute('id', newBalId);
+//     balanceDiv.innerHTML = `Your balance is $${currencyBalance}`;
+//     newAccountCard.appendChild(balanceDiv);
+
+//     let inputField = document.createElement('input');
+//     inputField.setAttribute('class', 'amountInput');
+//     inputField.setAttribute('type', 'text')
+
+//     let newInputId = newAccountName + 'input';
+//     inputField.setAttribute('id', newInputId);
+//     inputField.setAttribute('placeholder', 'enter amount');
+//     newAccountCard.appendChild(inputField);
+
+//     let brTag = document.createElement('br');
+//     newAccountCard.appendChild(brTag);
+
+//     let depositbutton = document.createElement('button');
+//     depositbutton.setAttribute('class', 'buttons');
+//     depositbutton.innerHTML = 'Deposit';
+//     depositbutton.value = 'deposit';
+//     newAccountCard.appendChild(depositbutton);
+
+
+//     let withdrawbutton = document.createElement('button');
+//     withdrawbutton.setAttribute('class', 'buttons');
+//     withdrawbutton.innerHTML = 'Withdraw';
+//     withdrawbutton.value = 'withdraw';
+//     newAccountCard.appendChild(withdrawbutton);
+
+//     let deletebutton = document.createElement('button');
+//     deletebutton.setAttribute('class', 'buttons');
+//     deletebutton.innerHTML = 'DELETE';
+//     deletebutton.value = 'delete';
+//     newAccountCard.appendChild(deletebutton);
+
+//     return newAccountCard;
+// },
+
+    }
 
 
 export default MyAccountComp
