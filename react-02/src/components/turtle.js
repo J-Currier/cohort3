6120374659bb671bfe5598ab0d8e5f828a0fa182
unknown <j.currier224@gmail.com>
2020-01-22 13:27:02 -0700

    test

--- react-02/src/components/turtle.js
+++ react-02/src/components/turtle.js
@@ -62,7 +62,7 @@ function Example() {
 
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You clicked {count} times hello</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
