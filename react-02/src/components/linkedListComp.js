dc941a7c800eb94e61567e7f15937b4d275f8179
unknown <j.currier224@gmail.com>
2020-03-09 10:59:57 -0600

    added buttons to linkedList

--- react-02/src/components/linkedListComp.js
+++ react-02/src/components/linkedListComp.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'; 
-import { AppContext } from '../contextMaker';
+import { AppContext, ContextProvider } from '../contextMaker';
 
 const LinkedListComp = () => {
     const context = React.useContext(AppContext);
 
     const [subject, setSubject] = useState('');
     const [amount, setAmount] = useState('');
-    console.log(context)
 
     const addButtonFunction =(event) => {
       if (subject === "" || amount === "") {
@@ -22,6 +21,31 @@ const LinkedListComp = () => {
       event.preventDefault();
       }
     };
+    const moveToFirst = (event) =>{
+      context.linkedList.first();
+      context.handleStateChange([{state: "current", newState: 'context.linkedList.current'}])
+    };
+
+    const moveToLast = (event) =>{
+      context.linkedList.last();
+      context.handleStateChange([{state: "current", newState: 'context.linkedList.current'}]);
+    };
+
+    const goToNext = (event) =>{
+      context.linkedList.next(context.linkedList.current);
+      context.handleStateChange([{state: "current", newState: 'context.linkedList.current'}]);
+    };
+
+    const goToPrevious = (event) =>{
+      context.linkedList.previous(context.linkedList.current);
+      context.handleStateChange([{state: "current", newState: 'context.linkedList.current'}]);
+    };
+
+    const deleteItem = (event) => {
+      context.linkedList.delete(context.linkedList.current);
+      context.handleStateChange([{state: 'current', newState: 'context.linkedList.current'}]);
+    }
+
     
 
     const displayItems = (linkedList) => {
@@ -49,9 +73,22 @@ const LinkedListComp = () => {
 
     const ItemCard = (props) => (
       <div className='myAccountCards'>
-      <span className='accountName'> {props.itemNumber}. {props.node.item}.....{props.node.amount}</span>
+      <span className='accountName' > {props.itemNumber}. {props.node.item}.....{props.node.amount}</span>
       </div>
-    );
+      );
+  
+    const DisplayCurrent =(myList) => {
+      if (myList.linkedList.current) {
+      return(
+      <div className='myAccountCards'>
+      <span className='accountName'> {myList.linkedList.current.item}...{myList.linkedList.current.amount}</span>
+      </div>
+      )
+      } else {
+        console.log('num two')
+        return( 'num two')
+      }
+      }
 
 
     return(
@@ -69,9 +106,22 @@ const LinkedListComp = () => {
                 <button className='buttons' id='addNewNodeButton' onClick={addButtonFunction}>Add my item</button>
               </div>
             </div>
+            <div className='myAccountCards' id= 'linkedListControls'>
+              Current Item: 
+              <DisplayCurrent linkedList={context.linkedList}/>
+              <br/>
+              <button className='buttons' id='firstButton' onClick={moveToFirst}>Go to first item</button>
+              <button className='buttons' id='lastButton' onClick={moveToLast}>Go to last item</button>
+              <button className='buttons' id='nextButton' onClick={goToNext}>Next Item</button>
+              <button className='buttons' id='backButton' onClick={goToPrevious}>Previoius Item</button>
+              <button className='buttons' id='deleteButton' onClick={deleteItem}>Delete Item</button>
+
+            </div>
             <div className='myAccountCards'>
-              my items 
+              Items <br/>
               {displayItems(context.linkedList)}
+              <span className='accountName'> Total amount: {context.linkedList.total()}</span>
+
             </div>
             
         </div>    
